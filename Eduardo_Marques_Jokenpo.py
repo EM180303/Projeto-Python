f425bae5062058ad189c3b5a8515a60f2465de63
Edu Marques <e.marques180303@gmail.com>
2020-11-17 00:28:05 -0300

    Miniprojeto Python

--- Eduardo_Marques_Jokenpo.py
+++ Eduardo_Marques_Jokenpo.py
@@ -10,7 +10,7 @@ jokenpo.append('Tesoura')
 escolha = []
 escolha.append('x')
 rodada = []
-rodada.append('x')
+rodada.append(0)
 rodada.append(0)
 rodada.append(0)
 vencedor = False
@@ -40,15 +40,32 @@ while (vencedor == False):
 
     if (escolha[1] == escolha[2]):
      print('Empate')
+     rodada[0]+= 1
     elif (((escolha[1] == 1) and (escolha[2] == 3)) or ((escolha[1] == 2) and (escolha[2] == 1)) or ((escolha[1] == 3) and (escolha[2] == 2))): 
      print(nome[1],' ganhou essa rodada')
      rodada[1] += 1
+     rodada[0]+= 1
     elif (((escolha[2] == 1) and (escolha[1] == 3)) or ((escolha[2] == 2) and (escolha[1] == 1)) or ((escolha[2] == 3) and (escolha[1] == 2))): 
      print(nome[2],' ganhou essa rodada')
      rodada[2] += 1
+     rodada[0]+= 1
+
 
     if ((rodada[1] == 2) or (rodada[2] == 2)):
       vencedor = True
     else:
       vencedor = False
+
+os.system("cls")
+
+if(rodada[1] > rodada[2]):
+    print('O vencedor foi ',nome[1])
+    print('Placar: ')
+    print(nome[1],': ',rodada[1],' X ',nome[2],' : ',rodada[2])
+    print('Quantidade de rodadas: ',rodada[0])
+else:
+    print('O vencedor foi ',nome[2])
+    print('Placar: ')
+    print(nome[1],': ',rodada[1],' X ',nome[2],' : ',rodada[2])
+    print('Quantidade de rodadas: ',rodada[0])
 
