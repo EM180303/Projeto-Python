f289ac2aaf30ce7b4595658e6ff7da67421f6803
Edu Marques <e.marques180303@gmail.com>
2020-11-16 21:22:37 -0300

    Nomes

---
+++ Eduardo_Marques_Jokenpo.py
@@ -0,0 +1,8 @@
+nome = []
+print('***Bem vindos!***')
+print('Preparados para o Jokenpo? Sim?')
+#para que o nome do jogador 1 fique na lista com o indice 1 e o nome do jogador 2 no 2
+nome.append('x')
+nome.append (str(input('Então informe o nome do jogador 1: ')))
+nome.append (str(input('Agora o nome do jogador 2: ')))
+
